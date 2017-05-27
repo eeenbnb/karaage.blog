b5ac84cf0b5960946f146a46f48627e7ac0c9822
eeenbnb <eee.kz.www@gmail.com>
2017-05-27 20:21:05 +0900

    update

--- js/common.js
+++ js/common.js
@@ -1,3 +1,20 @@
 $(function(){
+  var flag = 0;
+  $('.fa-chevron-down').click(function(){
+    flag = flag ? 0:1;
+    $("#links").animate( { height: 'toggle' }, 'slow' );
+    $('.fa-chevron-down').animate({zIndex:1},{
+	     duration:500,
+	      step:function(now){
+          if(flag){
+		        $(this).css({transform:'rotate(' + (now * 180) + 'deg)'});
+          }else{
+            $(this).css({transform:'rotate(' + (180 - (now * -180)) + 'deg)'});
+          }
+        },
+        complete:function(){
+		    }
+      });
 
+  });
 });
